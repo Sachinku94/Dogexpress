fa794c9bb33bf2783cfb633222f1fd517f6b4c12
Sachin Kumar <sachinkumar.primotech@gmail.com>
2025-01-06 20:01:14 +0530

    modified

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,14 @@
-# from Config.config_reader import read_config
-# import pytest
-# from selenium import webdriver
-
-
-# @pytest.fixture(scope="class")
-# def setup(request):
-#     base_url = read_config("URL", "base_url")
-#     driver = webdriver.Chrome()
-#     driver.get(base_url)
-#     # Add other setup steps here...
-#     request.cls.driver = driver
-#     yield
-#     driver.quit()
-
-
 from Config.config_reader import read_config
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    # Read the base URL from your configuration
     base_url = read_config("URL", "base_url")
-
-    # Initialize the Chrome driver using webdriver-manager
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome()
     driver.get(base_url)
-
-    # Assign the driver instance to the test class
+    # Add other setup steps here...
     request.cls.driver = driver
-
     yield
     driver.quit()

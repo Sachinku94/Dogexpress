909d61d71b23271074d003fac7ba28e571a70fd6
Sachin Kumar <sachinkumar.primotech@gmail.com>
2025-01-06 18:12:17 +0530

    modify

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,15 +32,30 @@ from Config.config_reader import read_config
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope="class")
 def setup(request):
+    # base_url = read_config("URL", "base_url")
+    # chrome_options = Options()
+    # driver = webdriver.Chrome(options=chrome_options)
+    # driver.get(base_url)
+    # # Add other setup steps here...
+    # request.cls.driver = driver
+    # yield
+    # driver.quit()
+
     base_url = read_config("URL", "base_url")
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(
+        "--remote-debugging-port=9222"
+    )  # Optional for debugging
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(base_url)
-    # Add other setup steps here...
     request.cls.driver = driver
     yield
     driver.quit()

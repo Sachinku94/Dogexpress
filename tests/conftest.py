ee3e50be3e35c3d5c325ed3d0af4b47d786b0772
Sachin Kumar <sachinkumar.primotech@gmail.com>
2025-01-06 21:47:08 +0530

    modified

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,28 @@
-from Config.config_reader import read_config
-import pytest
+# from Config.config_reader import read_config
+# import pytest
+# from selenium import webdriver
+# @pytest.fixture(scope="class")
+# def setup(request):
+#     base_url = read_config("URL", "base_url")
+#     driver = webdriver.Edge()
+#     driver.get(base_url)
+#     # Add other setup steps here...
+#     request.cls.driver = driver
+#     yield
+#     driver.quit()
 from selenium import webdriver
+from webdriver_manager.microsoft import EdgeDriverManager
+import pytest
+from Config.config_reader import read_config
 
 
 @pytest.fixture(scope="class")
 def setup(request):
     base_url = read_config("URL", "base_url")
-    driver = webdriver.Edge()
+
+    # Use WebDriverManager to automatically handle the Edge driver download and installation
+    driver = webdriver.Edge(EdgeDriverManager().install())
     driver.get(base_url)
-    # Add other setup steps here...
     request.cls.driver = driver
     yield
     driver.quit()
